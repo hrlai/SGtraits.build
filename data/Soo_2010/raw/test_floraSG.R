23da2c63df58e4086f8664cc0536df1bbfb22709
Chong Kwek Yan <kwekings@gmail.com>
2025-09-09 17:15:49 +0800

    Added script to strip out authorships and also synonymy matching with floraSG

--- data/Soo_2010/raw/test_floraSG.R
+++ data/Soo_2010/raw/test_floraSG.R
@@ -20,12 +20,74 @@ Soo_2010 <- extract_dataset(SGtraits, "Soo_2010")
 # extract taxa in Soo 2010
 taxa_test <- Soo_2010$taxa$taxon_name
 
-
-
-
 # Match with floraSG ------------------------------------------------------
 
 # Kwek Yan please help match taxa_test to floraSG nomenclature
 # in principal most if not all should have a match because Soo Wai Kit
 # followed Chong et al. 2009?
 # Delete this comment after reading
+
+# The following lines of code are modified from R/splitnames.R in the
+# floraSG repo
+# Summary of steps:
+# neutralize the filial f. to avoid confusion with forma
+# split forma by " f. "
+# split variety by " var. "
+# split subspecies by " subsp. "
+# split the remaining first part by " " and save the first element as genus
+# save the rest as specific epithet with authorities
+# split all the epithets and authors
+#  reinstate filial f.
+
+reinstate_f <- function(x) str_replace_all(x, c("Hallier_f." = "Hallier f.",
+                                                "Forsyth_f." = "Forsyth f."))
+
+taxa_test_df <- taxa_test %>%
+    str_replace_all(c(
+        "Hook. f." = "Hook.f.",
+        "L. f." = "L.f.",
+        "Rchb. f." = "Rchb.f.",
+        "Burm. f." = "Burm.f.",
+        "Hallier f." = "Hallier_f.",
+        "Forsyth f." = "Forsyth_f.",
+        "ssp." = "subsp."
+        )) %>%
+    data.frame(fullName_wAuth = .) %>%
+    separate(fullName_wAuth, into = c("allElse", "forma_wAuth"),
+             sep = " f. ", remove = FALSE) %>%
+    separate(allElse, into = c("allElse", "variety_wAuth"),
+             sep = " var. ", remove = TRUE) %>%
+    separate(allElse, into = c("allElse", "subspecies_wAuth"),
+             sep = " subsp. ", remove = TRUE) %>% 
+    mutate(genus = str_split_i(allElse, " ", 1)) %>%
+    mutate(species_wAuth = str_replace(allElse, paste0(genus, " "), "")) %>%
+    select(-allElse) %>%
+    mutate(species_woAuth = str_split_i(species_wAuth, " ", 1),
+           subspecies_woAuth = str_split_i(subspecies_wAuth, " ", 1),
+           variety_woAuth = str_split_i(variety_wAuth, " ", 1),
+           forma_woAuth = str_split_i(forma_wAuth, " ", 1)) %>%
+    mutate(fullName_woAuth = paste(genus, species_woAuth,
+                                   "subsp.", subspecies_woAuth,
+                                   "var.", variety_woAuth,
+                                   "f.", forma_woAuth)) %>%
+    mutate(fullName_woAuth = str_replace_all(fullName_woAuth,
+                                             c(" subsp. NA " = " ",
+                                               " var. NA " = " ",
+                                               " f. NA" = ""))) %>%
+    mutate(across(contains("_wAuth"), reinstate_f))
+
+# Match to floraSG nomenclature
+
+## floraSG repo is in a sister folder to SGtraits.build
+## yours might vary
+## ideal is to get floraSG published as a package so it can be loaded as one
+here <- getwd()
+setwd("../floraSG")
+source("./R/database_check.R")
+
+taxa_test_out <- database_check(taxa_test_df, colname_SppOnly = "fullName_woAuth")
+
+# check with the original data
+
+taxa_test_out2 <- database_check(data.frame(fullName_wAuth = taxa_test),
+               colname_SppAuthor = "fullName_wAuth")
