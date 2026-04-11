d69938a3c8ff548f492efc317af734c2b1e1b852
Hao Ran Lai <hrlai.ecology@gmail.com>
2026-04-11 11:30:13 +0800

    design pacakge logo

---
+++ R/logo.R
@@ -0,0 +1,17 @@
+library(hexSticker)
+
+sticker(
+  "inst/figures/tembusu.png",
+  package = "SGtraits",
+  p_y = 1,
+  p_size = 35,
+  p_color = "white",
+  s_x = 1.1,
+  s_y = 1,
+  s_width = 0.55,
+  h_fill = "#EE2536",
+  h_color = "white",
+  filename = "inst/figures/logo.png",
+  asp = 1,
+  dpi = 300
+)
